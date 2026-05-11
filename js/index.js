ef4701323da22404f1b554da35e2da13fc929f4a
Pedro Henrique <seu_email_do_github@example.com>
2026-05-11 12:47:36 -0300

    aprendendo javascript

--- js/index.js
+++ js/index.js
@@ -1,23 +1,2 @@
 
-/* 
 
-Vamos entender variáveis 
-variáveis são "recipientes" onde podemos armazenar informações que podem variar
-, ou seja, podem ter qualquer tipo de valor 
-
-
-no js temos 3 palavras-chaves para declarar variáveis 
-
--> var  
--> let
--> const 
-
-*/ 
-
-const x = 10; // 10
-
-{
-    const x = 2; //aqui é 2 
-} 
-
-document.getElementById("texto").innerHTML = x 
