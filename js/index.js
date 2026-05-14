107b0d74a58587e9690abf37e6196320d80bea16
Pedro Henrique da Silva Vasconcelos <pedro60976326@edu.df.senac.br>
2026-05-14 17:21:25 -0300

    eventos

--- js/index.js
+++ js/index.js
@@ -1,2 +1,67 @@
+/*EVENTOS
+
+Eventos são ações disparadas pela interação dos usuários na página.
+
+É o correto manejo desses eventos que tornam as páginas interativas e dinâmicas.
+
+Existem muitos eventos. Veja os mais utilizados:
+
+Lista de Eventos Comuns
+onclick -> Disparado quando recebe um click.
+
+ondblclick -> Disparado quando clique duplo.
+
+onmouseover -> Disparado quando o mouse está sobre.
+
+onmouseout -> Disparado quando o mouse é movido para fora do elemento.
+
+onmousemove -> Disparado quando o mouse é movido no elemento.
+
+onmousedown -> Disparado quando o clique do botão foi pressionado.
+
+onmouseup -> Disparado quando o clique do botão é liberado.
+
+onfocus -> Disparado quando o elemento recebe o foco. Válido para input, s...
+
+onchange -> Disparado quando existe uma mudança no conteúdo. "Ao mudar".
+
+onblur -> Disparado quando o elemento perde o foco. 
+
+onkeydown -> Disparado quando uma tecla é pressionada.
+
+onkeypress -> Disparado quando uma tecla é pressionada e solta.
+
+onkeyup -> Disparado quando uma tecla é solta sobre um elemento.
+
+onload -> Disparado quando a página terminou de ser carregada. Body.
+
+onresize -> Disparado quando há um redimencionamento da janela.
+
+*/  
+
+function eventoClick(){
+    alert('Acionou um evento de click') 
+    document.body.style.backgroundColor = "yellow"
+}
+
+function eventoDblClick(){
+    alert('Evento de click duplo')
+}  
+
+function viraVermelho() {
+
+    let div = document.getElementById("teste") 
+    div.style.backgroundColor = "red";
+} 
+
+function viraAzul() {
+
+    let div = document.getElementById("teste") 
+    div.style.backgroundColor = "blue";
+} 
+
+function limpaTexto() {
+    document.getElementById("campoTexto").value = "";
+}
 
 
